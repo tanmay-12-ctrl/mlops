f333f4c2ce6291d2b27ceee4069f97d399e92e26
Tanmay Swain <175398734+tanmay-12-ctrl@users.noreply.github.com>
2025-10-02 19:41:01 +0530

    done for file setup

--- setup.py
+++ setup.py
@@ -3,26 +3,42 @@ from typing import List
 
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
-   
 
 __version__ = "0.0.4"
 REPO_NAME = "mlops"
-PKG_NAME= "databaseautomation"
+PKG_NAME = "Nosql_mongodb.connect"
 AUTHOR_USER_NAME = "tanmay-12-ctrl"
 AUTHOR_EMAIL = "tanmayswain04@gmail.com"
 
 setup(
     name=PKG_NAME,
     version=__version__,
-    author=tanmay-12-ctrl,
-    author_email=tanmayswain04@gmail.com,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
     description="A python package for connecting with database.",
     long_description=long_description,
-    long_description_content="text/markdown",
+    long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    )
+    install_requires=[
+        "pymongo",
+        "pymongo[srv]",
+        "dnspython",
+        "pandas",
+        "numpy",
+        "ensure"
+    ],
+    extras_require={
+        "dev": [
+            "pytest==7.1.3",
+            "tox==3.25.1",
+            "black==22.8.0",
+            "flake8==5.0.4",
+            "mypy==0.971"
+        ]
+    }
+)
--- template.py
+++ template.py
@@ -31,4 +31,4 @@ for filepath in list_of_files:
 
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
-            pass # create an empty fi
+            pass # create an empty file
